6e3c687cee4f94b2a7fa53db8dca51537c9c1399
Kelho <Kelho88@pm.me>
2023-10-31 16:29:20 +0000

    changed the comments on the "check for errors"

--- include/push_swap.h
+++ include/push_swap.h
@@ -11,11 +11,10 @@
 //							Check for errors						 //
 ///////////////////////////////////////////////////////////////////////
 
-/**
- * This file contains the declarations of functions used to check for errors in the input arguments of the push_swap program.
- * These functions are used to validate the input arguments before processing them further.
- * The functions check for errors such as invalid characters, duplicates, and out of range values.
- */
+//This contains the declarations of functions used to check for errors in the input arguments of the push_swap program.
+//These functions are used to validate the input arguments before processing them further.
+//The functions check for errors such as invalid characters, duplicates, and out of range values.
+
 int	check_errors(int argc, char **argv);
 int check_number_n_space_str(char *str);
 int	check_max_n_min_str(char *str);
